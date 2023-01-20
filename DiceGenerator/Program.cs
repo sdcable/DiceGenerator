782406f2d6a54e7001bc1d7bda8ac169c43b8423
msajkld <sdcable18@gmail.com>
2023-01-20 10:47:35 -0700

    Added more comments

--- DiceGenerator/Program.cs
+++ DiceGenerator/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//Spencer Cable
+//Project to simulate dice rolls.
+
+using System;
 
 namespace RandomDiceGenerator
 {
@@ -6,10 +9,10 @@ namespace RandomDiceGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the dice throwing simulation!");
+            Console.WriteLine("Welcome to the dice throwing simulation!"); //Welcomes the user
             Console.Write("Enter how many dice rolls you desire: ");
             string text;
-            text = Console.ReadLine();
+            text = Console.ReadLine(); //Receives users text for how many dice rolls
             int numDiceRolls = Convert.ToInt32(text);
 
             int[] array2 = new int[11]; //This indicates that there will be 6 elements in this array, we just don't know what it is yet. 
@@ -19,46 +22,46 @@ namespace RandomDiceGenerator
                 array2[iCount] = 0;
             }
 
-            Random r = new Random();
+            Random r = new Random(); //Creates a new instance of random
 
             for (int i = 0; i < numDiceRolls; i++)
             {
 
-                int roll = r.Next(1, 7);
-                int roll2 = r.Next(1, 7);
+                int roll = r.Next(1, 7); //first dice roll
+                int roll2 = r.Next(1, 7); //second dice roll
 
-                int ultimateRoll = roll + roll2;
+                int ultimateRoll = roll + roll2; //combines both dice
 
-                array2[ultimateRoll - 2]++;
+                array2[ultimateRoll - 2]++; //add 1 to the number rolled
                 //array2[roll] = array2[roll] + 1; //These two lines are the same
 
             }
 
-            string[] starsArray = new string[11];
+            string[] starsArray = new string[11]; //creates new array
 
             for (int i = 0; i < starsArray.Length; i++) // for loop to set new array with the stars
             {
-                int numOfAstrix = (array2[i] * 100) / numDiceRolls;
+                int numOfAstrix = (array2[i] * 100) / numDiceRolls; //Creates the percent
 
                 if (numOfAstrix > 0)
                 {
                     for (int aCount = 0; aCount < numOfAstrix; aCount++)
                     {
-                        starsArray[i] = starsArray[i] + "*";
+                        starsArray[i] = starsArray[i] + "*"; //the amount of * will per 1% will be added.
                     }
                 }
             }
 
-            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS"); //output
             Console.WriteLine("Each \"*\" represents 1% of the total number of rolls");
             Console.WriteLine("Total number of rolls = " + numDiceRolls);
 
             for (int iCount = 0; iCount < starsArray.Length; iCount++) //Changing each instance of the array to be 0
             {
-                Console.WriteLine((iCount + 2) + ": " + starsArray[iCount]);
+                Console.WriteLine((iCount + 2) + ": " + starsArray[iCount]); //output
             }
 
-            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!"); //finish
 
         }
     }
